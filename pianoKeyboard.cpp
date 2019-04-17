8be8d4da7ae8ece9e0d1d5fc47b5cdc875306c85
Steve Platt <steveplatt@comcast.net>
2019-04-16 23:19:05 -0400

    Base version -- compiles

--- pianoKeyboard.cpp
+++ pianoKeyboard.cpp
@@ -1,17 +1,14 @@
-// PianoAnalyzer
+// pianoKeyboard implemenation
 //  April 2019
 //  Simple analysis for cheap piano keyboard
 //  Two cables to the keyboard itself: 8 and 6 lines
 //  Four additional lines.
 
 //
-// Version 0.2:  Simple keyboard probe and Serial output test
-// Version 0.1:  Has enough capacity to test out tha we are picking the correct ports on the Mega
-//
 //  Current assumption:
 //    8 lines are response lines
 //    6 lines are strobes to keyboard
-//    4 lines are strobes to the keyboard unit's "rhythm" etc. buttons
+//    45 lines are strobes to the keyboard unit's "rhythm" etc. buttons
 
 //
 // Coded for a mega.  We would have liked to use a Nano, but the digital line
@@ -22,45 +19,45 @@
 //    D22-D29 (port PA, 0-7): eight data return lines
 //    D30-D33 (port PC, 7-4): four strobe lines for control keys
 //    D42-D47 (port PL, 7-2): six strobe lines for keyboard
+#include <arduino.h>
+#include "pianoKeyboard.h"
 
-#include "Streaming.h"
+static int dataPorts[] = {22, 23, 24, 25, 26, 27, 28, 29, -1};
+static int keyStrobePorts[] = {42, 43, 44, 45, 46, 47, -1};
+static int buttonStrobePorts[] = {30, 31, 32, 33, 34, -1};
 
-int dataPorts[] = {22, 23, 24, 25, 26, 27, 28, 29, -1};
-int keyStrobePorts[] = {42, 43, 44, 45, 46, 47, -1};
-int buttonStrobePorts[] = {30, 31, 32, 33, 34, -1};
-
-void setup() {
-  // Set mode on all ports
-  Serial.begin(9600);
-  Serial << "Running." << endl;
-  initDataLines(dataPorts);
-  initStrobeLines(keyStrobePorts);
-  initStrobeLines(buttonStrobePorts);
-
-}
-
-void initDataLines(int lines[]) {
-  // lines is a -1-terminated array of ports.  They are set to input.
-  for(int i=0; lines[i]>=0; i++)
-    pinMode(lines[i], INPUT_PULLUP);
-}
-
-void initStrobeLines(int lines[]) {
+static void initStrobeLines(int lines[]) {
   // lines is a -1-terminated array of ports.  They are set to output.
   for(int i=0; lines[i]>=0; i++) {
     pinMode(lines[i], OUTPUT);
     digitalWrite(lines[i],HIGH);
   }
 }
+static void initDataLines(int lines[]) {
+  // lines is a -1-terminated array of ports.  They are set to input.
+  for(int i=0; lines[i]>=0; i++)
+    pinMode(lines[i], INPUT_PULLUP);
+}
+
+PianoKeyboard::PianoKeyboard(): m_keyDown(false), m_key(KNONE), m_keyChanged(false) {
+  initDataLines(dataPorts);
+  initStrobeLines(keyStrobePorts);
+  initStrobeLines(buttonStrobePorts);
+}
+
+inline int kbencode(int strobe, int dataLine) { return (strobe<<8) + dataLine; }
 
 // Strobe a set of strobes and report on the keys pressed.
 // Only one key pressed at a time
-bool keyPressed = false;
-int lastKey = 0;
-inline int kbencode(int strobe, int dataLine) { return (strobe<<8) + dataLine; }
-void checkKeys(char prefix, int strobePorts[]) {
+//bool keyPressed = false;
+//int lastKey = 0;
+//bool keyChanged;
+
+void PianoKeyboard::checkKeys(char prefix, int strobePorts[]) {
   int stat;
   int thisKey;
+
+  m_keyChanged = false;
   // perform one cycle of strobing all strobe lines and sensing all data lines for activity
   for(int strobe=0; strobePorts[strobe]>=0; strobe++) {
     // Force the strobe low to actiate it, then check all of the data lines
@@ -69,28 +66,39 @@ void checkKeys(char prefix, int strobePorts[]) {
       // checking data lines...
       stat=digitalRead(dataPorts[dataLine]);
       thisKey = kbencode(strobePorts[strobe],dataPorts[dataLine]);
-      if(stat==LOW && !keyPressed) {
+      if(stat==LOW && !m_keyDown) {
         // key hit, save value, mark as "hit", report results
-        keyPressed = true;
-        lastKey = thisKey;
+//        keyPressed = true;
+//        lastKey = thisKey;
+//        keyChanged = true;
+	    m_key = thisKey;
+	    m_keyDown = true;
+	    m_keyChanged = true;
+#if defined(DEBUG)
         Serial << "Down: " << prefix << strobe << " " << dataLine << " " << thisKey
 //          << (stat==LOW?" LOW ":" HIGH ") << (keyPressed?"pressed ":"!pressed ")
 //          << lastKey << " " << thisKey
           << endl;
-      } else if(stat==HIGH && keyPressed && thisKey==lastKey) {
+#endif
+      } else if(stat==HIGH && m_keyDown && thisKey==m_key) {
         // key released, mark as unpressed and report the (saved) key as being up
-        keyPressed = false;
+        m_keyDown = false;
+        m_keyChanged = true;
+#if defined(DEBUG)
         Serial << "Up: " << prefix << strobe << " " << dataLine
  //         << (stat==LOW?" LOW ":" HIGH ") << (keyPressed?"pressed ":"!pressed ")
  //         << lastKey << " " << thisKey
           << endl;
+#endif
       }
     } // end for dataPort
     digitalWrite(strobePorts[strobe], HIGH);
   } // end for strobe
 }
 
-void loop() {
-  checkKeys('K',keyStrobePorts);
-  checkKeys('B',buttonStrobePorts);
-}
+//void loop() {
+//  checkKeys('K',keyStrobePorts);
+//  checkKeys('B',buttonStrobePorts);
+//}
+
+
---
+++ pianoKeyboard.h
@@ -0,0 +1,53 @@
+//
+// Header for piano keyboard
+//
+
+class PianoKeyboard {
+	public:
+	enum KeyVals {
+		// buttons, have no correspondance to anything real
+		BT1, BT2, BT3, BT4, BT5, BT6, BT7, BT8, BVolUp, BVolDn,
+		BDog, BFrog, BDuck, BBird, BNoAnimal,
+		BStop, BDEMOA, BDEMOC, BRec, BPlay,
+		BTempoUp, BTempoDn, BR1, BR2, BR3, BR4, BR5, BR6, BR7, BR8,
+		// keyboard keys, values correspond to MIDI note numbers
+		KF2=41, KFS2, KG2, KGS2, KA2, KAS2, KB2,
+		KC3, KCS3, KD3, KDS3, KE3, KF3, KFS3, KG3, KGS3, KA3, KAS3, KB3,
+		KC4, KCS4, KD4, KDS4, KE4, KF4, KFS4, KG4, KGS4, KA4, KAS4, KB4,
+		KC5, KCS5, KD5, KDS5, KE5, KF5,
+		KNONE=99
+	};
+	private:
+	bool m_keyDown;
+	bool m_keyChanged;
+	KeyVals m_key;
+	struct KeyTranslations { int keycode; KeyVals keyval; };
+	static KeyTranslations keyTranslations[1] = {
+/*
+		{7704, BDEMOA}, {7705,BNoAnimal}, {7706,BBird}, {7707,BDuck}, {7708,BFrog}, {7709,BDog},		// B0
+		{8214,BT8}, {8215,BT7}, {8216,BT6}, {8217,BT5}, {8218,BT4}, {8219,BT3}, {8220,BT2}, {8221,BT1},	// B2
+		{8471,BPlay},{8472,BRec},{8473,BStop},{8474,BVolDn},{8475,BVolUp},{8476,BTempoDn},{8477,BTempoUp},	// B3
+		{8726,BR8}, {8727,BR7}, {8728,BR6}, {8729,BR5}, {8730,BR4}, {8731,BR3}, {8732,BR2}, {8733, BR1},	// B4
+		{10774,KD5}, {10775,KCS5}, {11305,KF5}, {11306,KE5}, {11307,KDS5},										// K0, K1
+		{11286,KC5}, {11287,KB4}, {11288,KAS4}, {11289,KA4}, {11290,KGS4}, {11291,KG4}, {11292,KFS4}, {11293,KF4},	// K2
+		{11542,KE4}, {11543,KDS4}, {11544,KD4}, {11545,KCS4}, {11546,KC4}, {11547,KB3}, {11548,KAS3}, {11549,KA3},	// K3
+		{11798,KGS3}, {11799,KG3}, {11800,KFS3}, {11801,KF3}, {11802,KE3}, {11803,KDS3}, {11804,KD3}, {11805,KCS3},	// K4
+		{12054,KC3}, {12055,KB2}, {12056,KAS2}, {12057,KA2}, {12058,KGS2}, {12059,KG2}, {12060,KFS2}, {12061,KF2},
+*/		{32767,KNONE}
+	};
+
+	KeyVals keycodeToKeyVal(int keycode){
+		int i;
+		for(i=0; keyTranslations[i].keyval!=KNONE; i++) {
+			if(keycode==keyTranslations[i].keycode) break;
+		}
+		return keyTranslations[i].keyval;
+	}
+	public:
+	PianoKeyboard();
+	bool pollForEvent() { return m_keyChanged; }
+	bool keyDown() { return m_keyDown; }
+	KeyVals key() { return m_key; }
+	private:
+	void checkKeys(char, int[]);
+};
