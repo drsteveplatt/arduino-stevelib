fd65e7b4b9d65e167e67c84f062e17de7a7932a1
Steve Platt <steveplatt@comcast.net>
2019-07-07 12:15:58 -0400

    Manages BigFun, Casio, including switch panel.
    Analyzer now handles both BigFun and Casio keyboards with documented wirings.  All buttons on BigFun and the switch panel on the Casio are handled.  Still only one button at a time.

--- pianoKeyboard.h
+++ pianoKeyboard.h
@@ -2,19 +2,39 @@
 // Header for piano keyboard
 //
 
+#ifdef BIGFUNKEYBOARD
+#define NUMBUTTONS	30
+#define NUMKEYS		37
+#endif
+#ifdef CASIOKEYBOARD
+#define NUMBUTTONS	24
+#define	NUMKEYS		44
+#endif
+
 class PianoKeyboard {
 	public:
 	enum KeyVals {
+#ifdef BIGFUNKEYBOARD
 		// buttons, have no correspondance to anything real
 		BT1, BT2, BT3, BT4, BT5, BT6, BT7, BT8, BVolUp, BVolDn,
 		BDog, BFrog, BDuck, BBird, BNoAnimal,
 		BStop, BDEMOA, BDEMOC, BRec, BPlay,
 		BTempoUp, BTempoDn, BR1, BR2, BR3, BR4, BR5, BR6, BR7, BR8,
+#endif
+#ifdef CASIOKEYBOARD
+		// button panel
+		B00, B01, B02, B03, B04, B05, B06, B07,		// all named B<strobe><sense>
+		B10, B11, B12, B13, B14, B15, B16, B17,
+		B20, B21, B22, B23, B24, B25, B26, B27,
+#endif
 		// keyboard keys, values correspond to MIDI note numbers
 		KF2=41, KFS2, KG2, KGS2, KA2, KAS2, KB2,
 		KC3, KCS3, KD3, KDS3, KE3, KF3, KFS3, KG3, KGS3, KA3, KAS3, KB3,
 		KC4, KCS4, KD4, KDS4, KE4, KF4, KFS4, KG4, KGS4, KA4, KAS4, KB4,
 		KC5, KCS5, KD5, KDS5, KE5, KF5,
+#ifdef CASIOKEYBOARD
+		KFS5, KG5, KGS5, KA5, KAS5, KB5, KC6,
+#endif
 		KNONE=99
 	};
 	private:
@@ -22,11 +42,16 @@ class PianoKeyboard {
 	bool m_keyChanged;
 	KeyVals m_key;
 	struct KeyTranslations { int keycode; KeyVals keyval; };
-	KeyTranslations keyTranslations[67] = {
-		{7704, BDEMOA}, {7705,BNoAnimal}, {7706,BBird}, {7707,BDuck}, {7708,BFrog}, {7709,BDog},		// B0: 6 items
-		{8214,BT8}, {8215,BT7}, {8216,BT6}, {8217,BT5}, {8218,BT4}, {8219,BT3}, {8220,BT2}, {8221,BT1},	// B2: 8 items
-		{8471,BPlay},{8472,BRec},{8473,BStop},{8474,BVolDn},{8475,BVolUp},{8476,BTempoDn},{8477,BTempoUp},	// B3: 7 items
-		{8726,BR8}, {8727,BR7}, {8728,BR6}, {8729,BR5}, {8730,BR4}, {8731,BR3}, {8732,BR2}, {8733, BR1},	// B4: 8 items
+	KeyTranslations keyTranslations[NUMBUTTONS+NUMANIMALS+1] = {
+#ifdef BIGFUNKEYBOARD
+		{7702, BDog}, {7703,BFrog}, {7704,BDuck}, {7705,BBird}, {7706,BNoAnimal}, {7707,BDEMOA},	// B0: 6 items
+		{7958,BT1}, {7959,BT2}, {7960,BT3}, {7961,BT4}, {7962,BT5}, {7963,BT6}, {7964,BT7}, {7965,BT8},		// B1: 8 items
+		{8214, BTempoUp}, {8215, BTempoDn}, {8216, BVolUp}, {8217, BVolDn}, {8218, BStop}, 			// B2: 8 items
+			{8219, BRec}, {8220, BPlay}, {8221, BDEMOC},
+		{8470,BR1}, {8471,BR2}, {8472,BR3}, {8473,BR4}, {8474,BR5}, {8475,BR6}, {8476,BR7}, {8477, BR8},	// B4: 8 items
+#endif
+#ifdef CASIOKEYBOARD
+#endif
 		{10774,KD5}, {10775,KCS5}, {11035,KF5}, {11036,KE5}, {11037,KDS5},										// K0, K1: 5 items
 		{11286,KC5}, {11287,KB4}, {11288,KAS4}, {11289,KA4}, {11290,KGS4}, {11291,KG4}, {11292,KFS4}, {11293,KF4},	// K2: 8 items
 		{11542,KE4}, {11543,KDS4}, {11544,KD4}, {11545,KCS4}, {11546,KC4}, {11547,KB3}, {11548,KAS3}, {11549,KA3},	// K3: 8 items
