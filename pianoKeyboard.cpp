6a9ba5c419089a1c44803187226ee38063e60d1c
Steve Platt <steveplatt@comcast.net>
2019-07-07 16:46:55 -0400

    Full event driving for pianokeyboard

--- pianoKeyboard.cpp
+++ pianoKeyboard.cpp
@@ -19,7 +19,6 @@
 //    D22-D29 (port PA, 0-7): eight data return lines
 //    D30-D34 (port PC, 7-3): five strobe lines for control buttons
 //    D42-D47 (port PL, 7-2): six strobe lines for keyboard
-#include <arduino.h>
 #include "pianoKeyboard.h"
 
 static int dataPorts[] = {22, 23, 24, 25, 26, 27, 28, 29, -1};
@@ -43,20 +42,33 @@ PianoKeyboard::PianoKeyboard(): m_keyDown(false), m_key(KNONE), m_keyChanged(fal
   initDataLines(dataPorts);
   initStrobeLines(keyStrobePorts);
   initStrobeLines(buttonStrobePorts);
+  for(int i=0; i<6; i++) theKeyBits[i] = 0;
+  for(int i=0; i<4; i++) theButtonBits[i] = 0;
 }
 
 
 inline int kbencode(int strobe, int dataLine) { return (strobe<<8) + dataLine; }
 
 // Strobe a set of strobes and report on the keys pressed.
-// Only one key pressed at a time
-//bool keyPressed = false;
-//int lastKey = 0;
-//bool keyChanged;
-
-void PianoKeyboard::checkKeys(int strobePorts[]) {
-  int stat;
+// New version, for multi-key tracking
+// Each call to checkKeys will check the appropriate set of ports.
+//   Each key state is checked against the previous key states.
+//   As soon as it finds a change, it
+//		sets m_keyChanged to true
+//		saves the key and the state
+//		updates the bit in bits
+//	If nothing is found, m_keyChanged will remain false.
+//
+// From the user's perspective:
+//		Call pollForEvent until an event is detected.  pollForEvent will call checkKeys.
+//		After an event is detected, use key() and keyDown() to figure out what the key
+//			is and how it has changed.
+//		Subsequent calls to pollForEvent() will clear the previously saved key/updown
+//			(they will not be available to key() and keyDown().  The states will still be
+//			preserved internally.
+void PianoKeyboard::checkKeys(int strobePorts[], byte* bits) {
   int thisKey;
+  byte stat;
 
   m_keyChanged = false;
   // perform one cycle of strobing all strobe lines and sensing all data lines for activity
@@ -67,15 +79,24 @@ void PianoKeyboard::checkKeys(int strobePorts[]) {
       // checking data lines...
       stat=digitalRead(dataPorts[dataLine]);
       thisKey = keycodeToKeyVal(kbencode(strobePorts[strobe],dataPorts[dataLine]));
-      if(stat==LOW && !m_keyDown) {
+      if(stat==LOW && !isSet(bits, strobe, dataLine)) {
         // key hit, save value, mark as "hit", report results
+Serial.print("[LOW "); Serial.print(strobePorts[strobe]); Serial.print(" ");
+Serial.print(dataPorts[dataLine]); Serial.print(" ");
+Serial.print(kbencode(strobePorts[strobe],dataPorts[dataLine])); Serial.println("]");
 	    m_key = thisKey;
 	    m_keyDown = true;
 	    m_keyChanged = true;
-      } else if(stat==HIGH && m_keyDown && thisKey==m_key) {
+	    setBit(bits, strobe, dataLine);
+      } else if(stat==HIGH && isSet(bits, strobe, dataLine)) {
         // key released, mark as unpressed and report the (saved) key as being up
-        m_keyDown = false;
+Serial.print("[HIGH "); Serial.print(strobePorts[strobe]); Serial.print(" ");
+Serial.print(dataPorts[dataLine]); Serial.print(" ");
+Serial.print(kbencode(strobePorts[strobe],dataPorts[dataLine])); Serial.println("]");
+		m_keyDown = false;
+		m_key = thisKey;
         m_keyChanged = true;
+        clearBit(bits, strobe, dataLine);
       }
     } // end for dataPort
     digitalWrite(strobePorts[strobe], HIGH);
@@ -83,9 +104,9 @@ void PianoKeyboard::checkKeys(int strobePorts[]) {
 }
 
 bool PianoKeyboard::pollForEvent() {
-	checkKeys(keyStrobePorts);
+	checkKeys(keyStrobePorts, theKeyBits);
 	if(m_keyChanged) return true;
-	checkKeys(buttonStrobePorts);
+	checkKeys(buttonStrobePorts, theButtonBits);
 	return m_keyChanged;
 }
 
--- pianoKeyboard.h
+++ pianoKeyboard.h
@@ -2,6 +2,8 @@
 // Header for piano keyboard
 //
 
+#define CASIOKEYBOARD
+
 #ifdef BIGFUNKEYBOARD
 #define NUMBUTTONS	30
 #define NUMKEYS		37
@@ -10,6 +12,8 @@
 #define NUMBUTTONS	24
 #define	NUMKEYS		44
 #endif
+
+#include <arduino.h>
 
 class PianoKeyboard {
 	public:
@@ -22,10 +26,10 @@ class PianoKeyboard {
 		BTempoUp, BTempoDn, BR1, BR2, BR3, BR4, BR5, BR6, BR7, BR8,
 #endif
 #ifdef CASIOKEYBOARD
-		// button panel
-		B00, B01, B02, B03, B04, B05, B06, B07,		// all named B<strobe><sense>
-		B10, B11, B12, B13, B14, B15, B16, B17,
-		B20, B21, B22, B23, B24, B25, B26, B27,
+		// switch panel
+		SW00, SW01, SW02, SW03, SW04, SW05, SW06, SW07,		// all named SW<strobe><sense>
+		SW10, SW11, SW12, SW13, SW14, SW15, SW16, SW17,
+		SW20, SW21, SW22, SW23, SW24, SW25, SW26, SW27,
 #endif
 		// keyboard keys, values correspond to MIDI note numbers
 		KF2=41, KFS2, KG2, KGS2, KA2, KAS2, KB2,
@@ -38,11 +42,20 @@ class PianoKeyboard {
 		KNONE=99
 	};
 	private:
+	// single-key-poll (old) version
 	bool m_keyDown;
 	bool m_keyChanged;
 	KeyVals m_key;
+	// event-driven (new) version
+	// track the reported states of the keys.  One byte for all of the Sense responses for each strobe
+	inline bool isSet(byte* bits, int strIndex, int bit) { return (bits[strIndex]&(1<<bit))!=0; }
+	inline bool setBit(byte* bits, int strIndex, int bit) { bits[strIndex] |= (1<<bit); }
+	inline bool clearBit(byte* bits, int strIndex, int bit) { bits[strIndex] &= (~(1<<bit)); }
+	byte theKeyBits[6];
+	byte theButtonBits[4];
+	// translating a key to a keycode
 	struct KeyTranslations { int keycode; KeyVals keyval; };
-	KeyTranslations keyTranslations[NUMBUTTONS+NUMANIMALS+1] = {
+	KeyTranslations keyTranslations[NUMBUTTONS+NUMKEYS+1] = {
 #ifdef BIGFUNKEYBOARD
 		{7702, BDog}, {7703,BFrog}, {7704,BDuck}, {7705,BBird}, {7706,BNoAnimal}, {7707,BDEMOA},	// B0: 6 items
 		{7958,BT1}, {7959,BT2}, {7960,BT3}, {7961,BT4}, {7962,BT5}, {7963,BT6}, {7964,BT7}, {7965,BT8},		// B1: 8 items
@@ -51,13 +64,17 @@ class PianoKeyboard {
 		{8470,BR1}, {8471,BR2}, {8472,BR3}, {8473,BR4}, {8474,BR5}, {8475,BR6}, {8476,BR7}, {8477, BR8},	// B4: 8 items
 #endif
 #ifdef CASIOKEYBOARD
+		{7702,SW00}, {7703,SW01}, {7704,SW02}, {7705,SW03}, {7706,SW04}, {7707,SW05}, {7708,SW06}, {7709,SW07},
+		{7958,SW10}, {7959,SW11}, {7960,SW12}, {7961,SW13}, {7962,SW14}, {7963,SW15}, {7964,SW16}, {7965,SW17},
+		{8214,SW20}, {8215,SW21}, {8216,SW22}, {8217,SW23}, {8218,SW24}, {8219,SW25}, {8220,SW26}, {8221,SW27},
 #endif
-		{10774,KD5}, {10775,KCS5}, {11035,KF5}, {11036,KE5}, {11037,KDS5},										// K0, K1: 5 items
-		{11286,KC5}, {11287,KB4}, {11288,KAS4}, {11289,KA4}, {11290,KGS4}, {11291,KG4}, {11292,KFS4}, {11293,KF4},	// K2: 8 items
-		{11542,KE4}, {11543,KDS4}, {11544,KD4}, {11545,KCS4}, {11546,KC4}, {11547,KB3}, {11548,KAS3}, {11549,KA3},	// K3: 8 items
-		{11798,KGS3}, {11799,KG3}, {11800,KFS3}, {11801,KF3}, {11802,KE3}, {11803,KDS3}, {11804,KD3}, {11805,KCS3},	// K4: 8 items
-		{12054,KC3}, {12055,KB2}, {12056,KAS2}, {12057,KA2}, {12058,KGS2}, {12059,KG2}, {12060,KFS2}, {12061,KF2}, 	// K5: 8 items
-		{32767,KNONE}																								// 1 item
+		{10774,KF2}, {10775,KFS2}, {10776,KG2}, {10777,KGS2}, {10778,KA2}, {10779,KAS2}, {10780,KB2}, {10781,KC3},	// Strobe0: 8 items
+		{11030,KCS3}, {11031,KD3}, {11032,KDS3}, {11033,KE3}, {11034,KF3}, {11035,KFS3}, {11036,KG3}, {11037,KGS3},	// Strobe1: 8 items
+		{11286,KA3}, {11287,KAS3}, {11288,KB3}, {11289,KC4}, {11290,KCS4}, {11291,KD4}, {11292,KDS4}, {11293,KE4},	// Strobe2: 8 items
+		{11542,KF4}, {11543,KFS4}, {11544,KG4}, {11545,KGS4}, {11546,KA4}, {11547,KAS4}, {11548,KB4}, {11549,KC5},	// Strobe3: 8 items
+		{11798,KCS5}, {11799,KD5}, {11800,KDS5}, {11801,KE5}, {11802,KF5}, {11803,KFS5}, {11804,KG5}, {11805,KGS5},	// Strobe4: 8 items
+		{12054,KA5}, {12055,KAS5}, {12056,KB5}, {12057,KC6},
+		{32767,KNONE}																				// 1 item
 	};
 
 	KeyVals keycodeToKeyVal(int keycode){
@@ -67,11 +84,13 @@ class PianoKeyboard {
 		}
 		return keyTranslations[i].keyval;
 	}
+
 	public:
 	PianoKeyboard();
 	bool pollForEvent();
 	bool keyDown() { return m_keyDown; }
 	KeyVals key() { return m_key; }
+
 	private:
-	void checkKeys(int[]);
+	void checkKeys(int* strobeports, byte* bits);
 };
